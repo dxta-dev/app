92aeca668d38efea9984fe229b24ac3d4ed854ab
Ivan Ivic <ivanivic842@gmail.com>
2023-12-07 11:54:19 +0100

    check if echo server works

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"fmt"
+
+	"net/http"
+
+	"github.com/labstack/echo/v4"
 )
 
 func main() {
 	fmt.Println("Hey there!")
-}
 
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
