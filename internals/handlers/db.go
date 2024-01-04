f4f66c8d2343775d5566adae326159ca4626d733
Ante Koceić <ante@crocoder.dev>
2024-01-04 12:00:46 +0100

    Adding types for dates and users

--- internals/handlers/db.go
+++ internals/handlers/db.go
@@ -6,7 +6,6 @@ import (
 	"dxta-dev/app/internals/templates"
 	"fmt"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/donseba/go-htmx"
@@ -17,61 +16,57 @@ import (
 	_ "modernc.org/sqlite"
 )
 
+type Date struct {
+	Day   int
+	Month int
+	Year  int
+}
+
+type User struct {
+	Name string
+}
+
 type JoinedIndexes struct {
-	Id                 int
-	MergedDay          int
-	MergedMonth        int
-	MergedYear         int
-	OpenedDay          int
-	OpenedMonth        int
-	OpenedYear         int
-	ClosedDay          int
-	ClosedMonth        int
-	ClosedYear         int
-	LastUpdatedDay     int
-	LastUpdatedMonth   int
-	LastUpdatedYear    int
-	StartedCodingDay   int
-	StartedCodingMonth int
-	StartedCodingYear  int
-	StartedPickupDay   int
-	StartedPickupMonth int
-	StartedPickupYear  int
-	StartedReviewDay   int
-	StartedReviewMonth int
-	StartedReviewYear  int
-	AuthorName         string
-	MergedByName       string
-	Approver1Name      string
-	Approver2Name      string
-	Approver3Name      string
-	Approver4Name      string
-	Approver5Name      string
-	Approver6Name      string
-	Approver7Name      string
-	Approver8Name      string
-	Approver9Name      string
-	Approver10Name     string
-	Committer1Name     string
-	Committer2Name     string
-	Committer3Name     string
-	Committer4Name     string
-	Committer5Name     string
-	Committer6Name     string
-	Committer7Name     string
-	Committer8Name     string
-	Committer9Name     string
-	Committer10Name    string
-	Reviewer1Name      string
-	Reviewer2Name      string
-	Reviewer3Name      string
-	Reviewer4Name      string
-	Reviewer5Name      string
-	Reviewer6Name      string
-	Reviewer7Name      string
-	Reviewer8Name      string
-	Reviewer9Name      string
-	Reviewer10Name     string
+	Id                int
+	MergedDate        Date
+	OpenedDate        Date
+	ClosedDate        Date
+	LastUpdatedDate   Date
+	StartedCodingDate Date
+	StartedPickupDate Date
+	StartedReviewDate Date
+	Author            User
+	MergedBy          User
+	Approver1         User
+	Approver2         User
+	Approver3         User
+	Approver4         User
+	Approver5         User
+	Approver6         User
+	Approver7         User
+	Approver8         User
+	Approver9         User
+	Approver10        User
+	Committer1        User
+	Committer2        User
+	Committer3        User
+	Committer4        User
+	Committer5        User
+	Committer6        User
+	Committer7        User
+	Committer8        User
+	Committer9        User
+	Committer10       User
+	Reviewer1         User
+	Reviewer2         User
+	Reviewer3         User
+	Reviewer4         User
+	Reviewer5         User
+	Reviewer6         User
+	Reviewer7         User
+	Reviewer8         User
+	Reviewer9         User
+	Reviewer10        User
 }
 
 func (a *App) Database(c echo.Context) error {
@@ -101,17 +96,17 @@ func (a *App) Database(c echo.Context) error {
 		return err
 	}
 
-	// Just for testing
-	repositoryId := 2
-	mrIds := []int{1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31}
-	var mrIdsInterface []interface{}
+	var attributes []interface{}
 
-	placeholderMrIdsSplice := make([]string, len(mrIds))
-	for i := range placeholderMrIdsSplice {
-		placeholderMrIdsSplice[i] = "?"
-		mrIdsInterface = append(mrIdsInterface, mrIds[i])
-	}
-	placeholderMrIds := strings.Join(placeholderMrIdsSplice, ", ")
+	// Just for testing
+	// Using date 27.07.2023. week 23
+	// start of week 24.07.2023.
+	// end of week 30.07.2023.
+
+	selectedDate := time.Date(2023, time.Month(7), 27, 0, 0, 0, 0, time.UTC)
+	year, week := selectedDate.ISOWeek()
+
+	attributes = append(attributes, week, year)
 
 	selectQuery := fmt.Sprintf(`
 		SELECT
@@ -210,19 +205,19 @@ func (a *App) Database(c echo.Context) error {
 		ON reviewer9.id = merge_request_fact_users_junk.reviewer9
 		JOIN forge_users AS reviewer10
 		ON reviewer10.id = merge_request_fact_users_junk.reviewer10
-		WHERE merge_request_metrics.merge_request IN (%s) 
-		AND merge_request_metrics.repository = %d
+		WHERE last_updated_date.week = ?
+		AND last_updated_date.year = ?
 		;
-	`, placeholderMrIds, repositoryId)
+	`)
 
 	mrStmt, err := db.Prepare(selectQuery)
 	if err != nil {
-		fmt.Println("US Error:", err)
+		fmt.Println("Error:", err)
 		return err
 	}
 	defer mrStmt.Close()
 
-	rows, err := mrStmt.Query(mrIdsInterface...)
+	rows, err := mrStmt.Query(attributes...)
 
 	if err != nil {
 		return err
@@ -239,80 +234,66 @@ func (a *App) Database(c echo.Context) error {
 
 		if err := rows.Scan(
 			&j.Id,
-			&j.MergedDay,
-			&j.MergedMonth,
-			&j.MergedYear,
-			&j.OpenedDay,
-			&j.OpenedMonth,
-			&j.OpenedYear,
-			&j.ClosedDay,
-			&j.ClosedMonth,
-			&j.ClosedYear,
-			&j.LastUpdatedDay,
-			&j.LastUpdatedMonth,
-			&j.LastUpdatedYear,
-			&j.StartedCodingDay,
-			&j.StartedCodingMonth,
-			&j.StartedCodingYear,
-			&j.StartedPickupDay,
-			&j.StartedPickupMonth,
-			&j.StartedPickupYear,
-			&j.StartedReviewDay,
-			&j.StartedReviewMonth,
-			&j.StartedReviewYear,
-			&j.AuthorName,
-			&j.MergedByName,
-			&j.Approver1Name,
-			&j.Approver2Name,
-			&j.Approver3Name,
-			&j.Approver4Name,
-			&j.Approver5Name,
-			&j.Approver6Name,
-			&j.Approver7Name,
-			&j.Approver8Name,
-			&j.Approver9Name,
-			&j.Approver10Name,
-			&j.Committer1Name,
-			&j.Committer2Name,
-			&j.Committer3Name,
-			&j.Committer4Name,
-			&j.Committer5Name,
-			&j.Committer6Name,
-			&j.Committer7Name,
-			&j.Committer8Name,
-			&j.Committer9Name,
-			&j.Committer10Name,
-			&j.Reviewer1Name,
-			&j.Reviewer2Name,
-			&j.Reviewer3Name,
-			&j.Reviewer4Name,
-			&j.Reviewer5Name,
-			&j.Reviewer6Name,
-			&j.Reviewer7Name,
-			&j.Reviewer8Name,
-			&j.Reviewer9Name,
-			&j.Reviewer10Name,
+			&j.MergedDate.Day, &j.MergedDate.Month, &j.MergedDate.Year,
+			&j.OpenedDate.Day, &j.OpenedDate.Month, &j.OpenedDate.Year,
+			&j.ClosedDate.Day, &j.ClosedDate.Month, &j.ClosedDate.Year,
+			&j.LastUpdatedDate.Day, &j.LastUpdatedDate.Month, &j.LastUpdatedDate.Year,
+			&j.StartedCodingDate.Day, &j.StartedCodingDate.Month, &j.StartedCodingDate.Year,
+			&j.StartedPickupDate.Day, &j.StartedPickupDate.Month, &j.StartedPickupDate.Year,
+			&j.StartedReviewDate.Day, &j.StartedReviewDate.Month, &j.StartedReviewDate.Year,
+			&j.Author.Name,
+			&j.MergedBy.Name,
+			&j.Approver1.Name,
+			&j.Approver2.Name,
+			&j.Approver3.Name,
+			&j.Approver4.Name,
+			&j.Approver5.Name,
+			&j.Approver6.Name,
+			&j.Approver7.Name,
+			&j.Approver8.Name,
+			&j.Approver9.Name,
+			&j.Approver10.Name,
+			&j.Committer1.Name,
+			&j.Committer2.Name,
+			&j.Committer3.Name,
+			&j.Committer4.Name,
+			&j.Committer5.Name,
+			&j.Committer6.Name,
+			&j.Committer7.Name,
+			&j.Committer8.Name,
+			&j.Committer9.Name,
+			&j.Committer10.Name,
+			&j.Reviewer1.Name,
+			&j.Reviewer2.Name,
+			&j.Reviewer3.Name,
+			&j.Reviewer4.Name,
+			&j.Reviewer5.Name,
+			&j.Reviewer6.Name,
+			&j.Reviewer7.Name,
+			&j.Reviewer8.Name,
+			&j.Reviewer9.Name,
+			&j.Reviewer10.Name,
 		); err != nil {
 			return err
 		}
 
-		approvers = append(approvers, j.Approver1Name, j.Approver2Name, j.Approver3Name, j.Approver4Name, j.Approver5Name, j.Approver6Name, j.Approver7Name, j.Approver8Name, j.Approver9Name, j.Approver10Name)
-		committers = append(committers, j.Committer1Name, j.Committer2Name, j.Committer3Name, j.Committer4Name, j.Committer5Name, j.Committer6Name, j.Committer7Name, j.Committer8Name, j.Committer9Name, j.Committer10Name)
-		reviewers = append(reviewers, j.Reviewer1Name, j.Reviewer2Name, j.Reviewer3Name, j.Reviewer4Name, j.Reviewer5Name, j.Reviewer6Name, j.Reviewer7Name, j.Reviewer8Name, j.Reviewer9Name, j.Reviewer10Name)
+		approvers = append(approvers, j.Approver1.Name, j.Approver2.Name, j.Approver3.Name, j.Approver4.Name, j.Approver5.Name, j.Approver6.Name, j.Approver7.Name, j.Approver8.Name, j.Approver9.Name, j.Approver10.Name)
+		committers = append(committers, j.Committer1.Name, j.Committer2.Name, j.Committer3.Name, j.Committer4.Name, j.Committer5.Name, j.Committer6.Name, j.Committer7.Name, j.Committer8.Name, j.Committer9.Name, j.Committer10.Name)
+		reviewers = append(reviewers, j.Reviewer1.Name, j.Reviewer2.Name, j.Reviewer3.Name, j.Reviewer4.Name, j.Reviewer5.Name, j.Reviewer6.Name, j.Reviewer7.Name, j.Reviewer8.Name, j.Reviewer9.Name, j.Reviewer10.Name)
 
 		metrics = append(metrics, templates.MergeRequestMetrics{
 			Id:              j.Id,
-			MergedAt:        time.Date(j.MergedYear, time.Month(j.MergedMonth), j.MergedDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			OpenedAt:        time.Date(j.OpenedYear, time.Month(j.OpenedMonth), j.OpenedDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			ClosedAt:        time.Date(j.ClosedYear, time.Month(j.MergedMonth), j.ClosedDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			LastUpdatedAt:   time.Date(j.LastUpdatedYear, time.Month(j.LastUpdatedMonth), j.LastUpdatedDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			StartedCodingAt: time.Date(j.StartedCodingYear, time.Month(j.StartedCodingMonth), j.StartedCodingDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			StartedPickupAt: time.Date(j.StartedPickupYear, time.Month(j.StartedPickupMonth), j.StartedPickupDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			StartedReviewAt: time.Date(j.StartedReviewYear, time.Month(j.StartedReviewMonth), j.StartedReviewDay, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
-			Author:          j.AuthorName,
-			MergedBy:        j.MergedByName,
+			MergedAt:        time.Date(j.MergedDate.Year, time.Month(j.MergedDate.Month), j.MergedDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			OpenedAt:        time.Date(j.OpenedDate.Year, time.Month(j.OpenedDate.Month), j.OpenedDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			ClosedAt:        time.Date(j.ClosedDate.Year, time.Month(j.OpenedDate.Month), j.ClosedDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			LastUpdatedAt:   time.Date(j.LastUpdatedDate.Year, time.Month(j.LastUpdatedDate.Month), j.LastUpdatedDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			StartedCodingAt: time.Date(j.StartedCodingDate.Year, time.Month(j.StartedCodingDate.Month), j.StartedCodingDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			StartedPickupAt: time.Date(j.StartedPickupDate.Year, time.Month(j.StartedPickupDate.Month), j.StartedPickupDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			StartedReviewAt: time.Date(j.StartedReviewDate.Year, time.Month(j.StartedReviewDate.Month), j.StartedReviewDate.Day, 0, 0, 0, 0, time.UTC).Format("Mon, 02-01-2006"),
+			Author:          j.Author.Name,
+			MergedBy:        j.MergedBy.Name,
 			Approvers:       approvers,
-			Committers:      reviewers,
+			Committers:      committers,
 			Reviewers:       reviewers,
 		})
 	}
