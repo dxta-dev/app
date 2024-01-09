13d2fb7d820c91ae936d56d38f1d693b5a21f572
dejan-crocoder <dejan@crocoder.dev>
2024-01-09 12:31:56 +0100

    LoadTenants function

--- internal/middlewares/tenant_middleware.go
+++ internal/middlewares/tenant_middleware.go
@@ -1,9 +1,46 @@
 package middlewares
 
 import (
-	"github.com/labstack/echo/v4"
+	"encoding/json"
+	"io"
+	"net/http"
+	"os"
 	"strings"
+
+	"github.com/labstack/echo/v4"
 )
+
+type TenantInfo struct {
+	Name string `json:"tenant"`
+}
+
+type TenantsConfig struct {
+	tenants []TenantInfo
+}
+
+// TODO: just key-value store map (tenant-key)->(db-connection)
+var tenantsConfig TenantsConfig
+
+func LoadTenants() error {
+	resp, err := http.Get(os.Getenv("OSS_TENANTS_ENPDOINT"))
+	if err != nil {
+		return err
+	}
+
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := json.Unmarshal(body, &tenantsConfig.tenants); err != nil {
+		return err
+	}
+
+	// TODO: open db connections ?
+	return nil
+}
 
 func TenantMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
