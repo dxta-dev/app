a99c93cd640546649d678945e5d60bda36eec408
David Abram <david@crocoder.dev>
2024-01-15 12:30:54 +0100

    toml

---
+++ internal/utils/config.go
@@ -0,0 +1,111 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/BurntSushi/toml"
+)
+
+
+type Tenant struct {
+	Name string `toml:"name"`
+	SubdomainName string `toml:"subdomain"`
+	DatabaseName string `toml:"database_name"`
+	DatabaseUrl *string `toml:"database_url"`
+}
+
+type Config struct {
+	IsMultiTenant bool
+	ShouldUseSuperDatabase bool
+	SuperDatabaseUrl *string
+	TenantUrlTemplate *string
+	Tenants map[string]Tenant
+}
+
+type TomlConfig struct {
+	TenantDatabaseUrlTemplate *string `toml:"tenant_database_url_template"`
+	TenantDatabaseGroupAuth *string `toml:"tenant_database_group_auth"`
+	SuperDatabaseUrl *string `toml:"super_database_url"`
+	Tenants map[string]Tenant `toml:"tenants"`
+}
+
+func ValidateConfig(config *TomlConfig) (*Config, error) {
+	var superDatabaseUrl *string
+
+	if config.SuperDatabaseUrl != nil {
+		superDatabaseUrl = config.SuperDatabaseUrl
+	}
+
+	for key, tenant := range config.Tenants {
+		if tenant.Name == "" {
+			tenant.Name = key
+		}
+		if tenant.SubdomainName == "" {
+			tenant.SubdomainName = key
+		}
+		if tenant.DatabaseName == "" {
+			tenant.DatabaseName = key
+		}
+
+		if tenant.DatabaseUrl == nil && config.TenantDatabaseUrlTemplate != nil {
+			databaseUrl := fmt.Sprintf(*config.TenantDatabaseUrlTemplate, tenant.DatabaseName)
+			tenant.DatabaseUrl = &databaseUrl
+		}
+
+		if tenant.DatabaseUrl != nil && config.TenantDatabaseGroupAuth != nil {
+			databaseUrl := *tenant.DatabaseUrl + "?auth_token=" + *config.TenantDatabaseGroupAuth
+			tenant.DatabaseUrl = &databaseUrl
+		}
+
+		config.Tenants[key] = tenant
+	}
+
+	shouldUseSuperDatabase := false
+
+	if superDatabaseUrl != nil && config.Tenants == nil {
+		shouldUseSuperDatabase = true
+	} else {
+		superDatabaseUrl = nil
+	}
+
+
+	isMultiTenant := false
+
+	if len(config.Tenants) > 1 {
+		isMultiTenant = true
+	}
+
+	return &Config{
+		IsMultiTenant: isMultiTenant,
+		ShouldUseSuperDatabase: shouldUseSuperDatabase,
+		SuperDatabaseUrl: superDatabaseUrl,
+		Tenants: config.Tenants,
+	}, nil
+}
+
+func GetConfig() (*Config, error) {
+	conf, _ := LoadConfigToml("config.toml")
+
+	config, err := ValidateConfig(conf)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
+
+func LoadConfigToml(path string) (*TomlConfig, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var config TomlConfig
+	if _, err := toml.Decode(string(data), &config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
---
+++ internal/utils/config_test.go
@@ -0,0 +1,341 @@
+package utils
+
+import (
+	"os"
+	"reflect"
+	"testing"
+)
+
+func createTempFile(content string) (*os.File, error) {
+	tmpfile, err := os.CreateTemp("", "example")
+	if err != nil {
+		return nil, err
+	}
+
+	if _, err := tmpfile.Write([]byte(content)); err != nil {
+		return nil, err
+	}
+
+	if err := tmpfile.Close(); err != nil {
+		return nil, err
+	}
+
+	return tmpfile, nil
+}
+
+func TestValidateConfig(t *testing.T) {
+	oneDatabaseUrl := "sqlite://one.db"
+	superDatabaseUrl := "sqlite://super.db"
+
+	dbTemplate := "sqlite://%s.db"
+	authToken := "auth_token"
+
+	databaseUrl := "sqlite://one.db?auth_token=auth_token"
+
+	tests := []struct {
+		name      string
+		config    *TomlConfig
+		expected  *Config
+		expectErr bool
+	}{
+		{
+			name: "simple_config",
+			config: &TomlConfig{
+				SuperDatabaseUrl:          nil,
+				TenantDatabaseUrlTemplate: nil,
+				TenantDatabaseGroupAuth:   nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseName: "one", DatabaseUrl: &oneDatabaseUrl},
+				},
+			},
+			expected: &Config{
+				IsMultiTenant:          false,
+				ShouldUseSuperDatabase: false,
+				SuperDatabaseUrl:       nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseName: "one", DatabaseUrl: &oneDatabaseUrl},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "should_set_super_database_url_to_nil",
+			config: &TomlConfig{
+				SuperDatabaseUrl:          &superDatabaseUrl,
+				TenantDatabaseUrlTemplate: nil,
+				TenantDatabaseGroupAuth:   nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseName: "one", DatabaseUrl: &oneDatabaseUrl},
+				},
+			},
+			expected: &Config{
+				IsMultiTenant:          false,
+				ShouldUseSuperDatabase: false,
+				SuperDatabaseUrl:       nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseName: "one", DatabaseUrl: &oneDatabaseUrl},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "should_set_database_url_to_tenants",
+			config: &TomlConfig{
+				SuperDatabaseUrl:          nil,
+				TenantDatabaseUrlTemplate: &dbTemplate,
+				TenantDatabaseGroupAuth:   &authToken,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseName: "one", DatabaseUrl: nil},
+				},
+			},
+			expected: &Config{
+				IsMultiTenant:          false,
+				ShouldUseSuperDatabase: false,
+				SuperDatabaseUrl:       nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseName: "one", DatabaseUrl: &databaseUrl},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "should_set_default_values",
+			config: &TomlConfig{
+				SuperDatabaseUrl:          nil,
+				TenantDatabaseUrlTemplate: nil,
+				TenantDatabaseGroupAuth:   nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "", SubdomainName: "", DatabaseName: "", DatabaseUrl: nil},
+				},
+			},
+			expected: &Config{
+				IsMultiTenant:          false,
+				ShouldUseSuperDatabase: false,
+				SuperDatabaseUrl:       nil,
+				Tenants: map[string]Tenant{
+					"tenant1": {Name: "tenant1", SubdomainName: "tenant1", DatabaseName: "tenant1", DatabaseUrl: nil},
+				},
+			},
+			expectErr: false,
+		},
+		{
+			name: "should_use_super_database",
+			config: &TomlConfig{
+				SuperDatabaseUrl:          &superDatabaseUrl,
+				TenantDatabaseUrlTemplate: &dbTemplate,
+				TenantDatabaseGroupAuth:   &authToken,
+				Tenants:                   nil,
+			},
+			expected: &Config{
+				IsMultiTenant:          true,
+				ShouldUseSuperDatabase: true,
+				SuperDatabaseUrl:       &superDatabaseUrl,
+				Tenants:                nil,
+			},
+			expectErr: false,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			cfg, err := ValidateConfig(test.config)
+			if err != nil && !test.expectErr {
+				t.Fatalf("Expected no error, got %v", err)
+			}
+
+			if !reflect.DeepEqual(cfg, test.expected) {
+				t.Errorf("Expected %+v, got %+v", test.expected, cfg)
+			}
+		})
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+
+	t.Run("valid_path_and_format", func(t *testing.T) {
+		content := `
+super_database_url = "sqlite://super.db"
+  [tenants.tenant1]
+  name = "Tenant One"
+  subdomain = "one"
+  database_url = "sqlite://one.db"
+`
+		tmpfile, err := createTempFile(content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name())
+
+		superDatabaseUrl := "sqlite://super.db"
+		tenant1DatabaseUrl := "sqlite://one.db"
+		expected := &TomlConfig{
+			SuperDatabaseUrl: &superDatabaseUrl,
+			Tenants: map[string]Tenant{
+				"tenant1": {Name: "Tenant One", SubdomainName: "one", DatabaseUrl: &tenant1DatabaseUrl},
+			},
+		}
+
+		cfg, err := LoadConfigToml(tmpfile.Name())
+		if err != nil {
+			t.Fatalf("Failed to load config: %v", err)
+		}
+
+		if !reflect.DeepEqual(cfg, expected) {
+			t.Errorf("Expected %+v, got %+v", expected, cfg)
+		}
+
+	})
+
+	t.Run("file_not_found", func(t *testing.T) {
+		_, err := LoadConfigToml("nonexistent.toml")
+		if err == nil {
+			t.Errorf("Expected error, got nil")
+		}
+	})
+
+	t.Run("bad_format", func(t *testing.T) {
+		content := `
+super_database_url = "sqlite://super.db"
+  [tenants.tenant1]
+  name = "Tenant One"
+  subdomain "one"
+  database_url = sqlite://one.db
+`
+		tmpfile, err := createTempFile(content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name())
+
+		_, err = LoadConfigToml(tmpfile.Name())
+		if err == nil {
+			t.Fatalf("Somehow the config is loaded successfully")
+		}
+	})
+
+	t.Run("real_life_example", func(t *testing.T) {
+		content := `
+super_database_url = "sqlite://super.db"
+
+[tenants.crocoder]
+  subdomain = "crocoder"
+  name = "crocoder"
+  database_url = "sqlite://crocoder.db"
+
+[tenants.acme]
+  database_url = "sqlite://acme.db"
+`
+		tmpfile, err := createTempFile(content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name())
+
+		superDatabaseUrl := "sqlite://super.db"
+		crocoderDatabaseUrl := "sqlite://crocoder.db"
+		acmeDatabaseUrl := "sqlite://acme.db"
+		expected := &TomlConfig{
+			SuperDatabaseUrl: &superDatabaseUrl,
+			Tenants: map[string]Tenant{
+				"crocoder": {Name: "crocoder", SubdomainName: "crocoder", DatabaseUrl: &crocoderDatabaseUrl},
+				"acme":     {DatabaseUrl: &acmeDatabaseUrl},
+			},
+		}
+
+		cfg, err := LoadConfigToml(tmpfile.Name())
+		if err != nil {
+			t.Fatalf("Failed to load config: %v", err)
+		}
+
+		if !reflect.DeepEqual(cfg, expected) {
+			t.Errorf("Expected %+v, got %+v", expected, cfg)
+		}
+
+	})
+
+	t.Run("empty_toml", func(t *testing.T) {
+		content := ""
+		tmpfile, err := createTempFile(content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name())
+
+		expected := &TomlConfig{}
+
+		cfg, err := LoadConfigToml(tmpfile.Name())
+		if err != nil {
+			t.Fatalf("Failed to load config: %v", err)
+		}
+
+		if !reflect.DeepEqual(cfg, expected) {
+			t.Errorf("Expected %+v, got %+v", expected, cfg)
+		}
+	})
+
+	t.Run("tenant_database_template_example", func(t *testing.T) {
+		content := `
+super_database_url = "sqlite://super.db"
+tenant_database_url_template = "sqlite://%s.db"
+`
+		tmpfile, err := createTempFile(content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name())
+
+		superDatabaseUrl := "sqlite://super.db"
+		tenantDatabaseUrlTemplate := "sqlite://%s.db"
+		expected := &TomlConfig{
+			SuperDatabaseUrl:          &superDatabaseUrl,
+			TenantDatabaseUrlTemplate: &tenantDatabaseUrlTemplate,
+		}
+
+		cfg, err := LoadConfigToml(tmpfile.Name())
+		if err != nil {
+			t.Fatalf("Failed to load config: %v", err)
+		}
+
+		if !reflect.DeepEqual(cfg, expected) {
+			t.Errorf("Expected %+v, got %+v", expected, cfg)
+		}
+
+	})
+
+	t.Run("tenant_database_template_group_auth_example", func(t *testing.T) {
+		content := `
+super_database_url = "sqlite://super.db"
+tenant_database_url_template = "sqlite://%s.db"
+tenant_database_group_auth = "group_auth"
+`
+		tmpfile, err := createTempFile(content)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		defer os.Remove(tmpfile.Name())
+
+		superDatabaseUrl := "sqlite://super.db"
+		tenantDatabaseUrlTemplate := "sqlite://%s.db"
+		groupAuth := "group_auth"
+		expected := &TomlConfig{
+			SuperDatabaseUrl:          &superDatabaseUrl,
+			TenantDatabaseUrlTemplate: &tenantDatabaseUrlTemplate,
+			TenantDatabaseGroupAuth:   &groupAuth,
+		}
+
+		cfg, err := LoadConfigToml(tmpfile.Name())
+		if err != nil {
+			t.Fatalf("Failed to load config: %v", err)
+		}
+
+		if !reflect.DeepEqual(cfg, expected) {
+			t.Errorf("Expected %+v, got %+v", expected, cfg)
+		}
+	})
+}
