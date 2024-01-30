760fe4385dbf96af8907dc71b922eda02e4f276f
David Abram <david@crocoder.dev>
2024-01-30 23:38:33 +0100

    cache-control & etag

--- internal/handlers/static.go
+++ internal/handlers/static.go
@@ -1,9 +1,14 @@
 package handlers
 
 import (
+	"crypto/md5"
+	"dxta-dev/app"
+	"fmt"
 	"io/fs"
 	"net/http"
-	"dxta-dev/app"
+	"strings"
+	"time"
+
 	"github.com/labstack/echo/v4"
 )
 
@@ -14,5 +19,40 @@ func PublicHandler() echo.HandlerFunc {
 		panic(err)
 	}
 
-	return echo.WrapHandler(http.FileServer(http.FS(publicFS)))
+	fileServer := http.FileServer(http.FS(publicFS))
+
+	return func(c echo.Context) error {
+		c.Response().Header().Set("Cache-Control", "public, max-age=31536000")
+
+		path := c.Request().URL.Path
+
+		path = "style.css"
+		file, err := publicFS.Open(path)
+		if err != nil {
+			return echo.NewHTTPError(http.StatusNotFound)
+		}
+
+		defer file.Close()
+
+		fileInfo, err := file.Stat()
+		if err != nil {
+			return echo.NewHTTPError(http.StatusNotFound)
+		}
+
+		lastModified := fileInfo.ModTime().Format(time.RFC1123)
+
+		etag :=  fmt.Sprintf("%x", md5.Sum([]byte(lastModified)))
+
+		if match := c.Request().Header.Get("If-None-Match"); match != "" {
+			if strings.Contains(match, etag) {
+				c.Response().WriteHeader(http.StatusNotModified)
+				return nil
+			}
+		}
+
+		c.Response().Header().Set("Etag", etag)
+
+		fileServer.ServeHTTP(c.Response(), c.Request())
+		return nil
+	}
 }
