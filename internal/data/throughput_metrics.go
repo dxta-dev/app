f4a0aa770cdbd29580a3adf3e405e406ec44594f
David Abram <david@crocoder.dev>
2024-02-07 15:15:25 +0100

    average mr size

---
+++ internal/data/throughput_metrics.go
@@ -0,0 +1,25 @@
+package data
+
+func (s *Store) GetTotalCodeChanges(weeks []string) (interface{}, error) {
+	return nil, nil
+}
+
+func (s *Store) GetTotalCommits(weeks []string) (interface{}, error) {
+	return nil, nil
+}
+
+func (s *Store) GetTotalPRsOpened(weeks []string) (interface{}, error) {
+	return nil, nil
+}
+
+func (s *Store) GetTotalReviews(weeks []string) (interface{}, error) {
+	return nil, nil
+}
+
+func (s *Store) GetMergeFrequency(weeks []string) (interface{}, error) {
+	return nil, nil
+}
+
+func (s *Store) GetDeployFrequency(weeks []string) (interface{}, error) {
+	return nil, nil
+}
