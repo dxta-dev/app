89b78f3a8559d890b7bed2672bcfa7fe41f97e9c
Dejan Tot <dejan@crocoder.dev>
2024-02-09 17:41:26 +0100

    feat: implement GetPRsMergedWithoutReview (#80)
    * implement GetPRsMergedWithoutReview
    
    * return type

--- internal/data/quality_metrics.go
+++ internal/data/quality_metrics.go
@@ -75,8 +75,64 @@ func (s *Store) GetAverageReviewDepth(weeks []string) (interface{}, error) {
 	return nil, nil
 }
 
-func (s *Store) GetPRsMergedWithoutReview(weeks []string) (interface{}, error) {
-	return nil, nil
+type PRCountByWeek struct {
+	Week  string
+	Count int
+}
+
+func (s *Store) GetPRsMergedWithoutReview(weeks []string) ([]PRCountByWeek, error) {
+	placeholders := strings.Repeat("?,", len(weeks)-1) + "?"
+
+	query := fmt.Sprintf(`
+	SELECT
+		COUNT(*),
+		mergedAt.week
+	FROM transform_merge_request_metrics as metrics
+	JOIN transform_merge_request_fact_dates_junk as dj
+	ON metrics.dates_junk = dj.id
+	JOIN transform_dates as mergedAt
+	ON dj.merged_at = mergedAt.id
+	WHERE mergedAt.week IN (%s) and metrics.review_depth = 0
+	GROUP BY mergedAt.week;`,
+		placeholders)
+
+	db, err := sql.Open("libsql", s.DbUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	weeksInterface := make([]interface{}, len(weeks))
+	for i, v := range weeks {
+		weeksInterface[i] = v
+	}
+
+	rows, err := db.Query(query, weeksInterface...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	var prCountByWeeks []PRCountByWeek
+
+	for rows.Next() {
+		var prweek PRCountByWeek
+
+		if err := rows.Scan(
+			&prweek.Count,
+			&prweek.Week,
+		); err != nil {
+			return nil, err
+		}
+
+		prCountByWeeks = append(prCountByWeeks, prweek)
+	}
+
+	return prCountByWeeks, nil
 }
 
 func (s *Store) GetNewCodePercentage(weeks []string) (interface{}, error) {
