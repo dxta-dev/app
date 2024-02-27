9a8011acde166855666b0f94c5fbba907f1791f4
Ivan Ivic <ivanivic842@gmail.com>
2024-02-27 13:34:02 +0100

    YAxis values and labels

--- internal/template/time_series_chart_component.go
+++ internal/template/time_series_chart_component.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"io"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/a-h/templ"
@@ -18,7 +19,30 @@ type TimeSeries struct {
 	Weeks   []string
 }
 
+func getYAxisValues(yValues []float64) []float64 {
+	if len(yValues) == 0 {
+		return []float64{0, 1, 2, 3, 4, 5}
+	}
+
+	highest := yValues[0]
+
+	for _, num := range yValues {
+		if num > highest {
+			highest = num
+		}
+	}
+
+	lowest := 0.0
+	percent25 := highest * 0.25
+	percent50 := highest * 0.5
+	percent75 := highest * 0.75
+
+	return []float64{lowest, percent25, percent50, percent75, highest}
+}
+
 func TimeSeriesChart(series TimeSeries) templ.Component {
+
+	YAxisValues := getYAxisValues(series.YValues)
 	mainSeries := chart.ContinuousSeries{
 		Style: chart.Style{
 			StrokeWidth: chart.Disabled,
@@ -37,6 +61,15 @@ func TimeSeriesChart(series TimeSeries) templ.Component {
 	}
 
 	graph := chart.Chart{
+		YAxis: chart.YAxis{
+			Ticks: []chart.Tick{
+				{Value: YAxisValues[0], Label: strconv.FormatFloat(YAxisValues[0], 'f', -1, 64)},
+				{Value: YAxisValues[1], Label: strconv.FormatFloat(YAxisValues[1], 'f', -1, 64)},
+				{Value: YAxisValues[2], Label: strconv.FormatFloat(YAxisValues[2], 'f', -1, 64)},
+				{Value: YAxisValues[3], Label: strconv.FormatFloat(YAxisValues[3], 'f', -1, 64)},
+				{Value: YAxisValues[4], Label: strconv.FormatFloat(YAxisValues[4], 'f', -1, 64)},
+			},
+		},
 		Series: []chart.Series{
 			lineSeries,
 			mainSeries,
