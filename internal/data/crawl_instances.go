7d357fdae9169bde085e665e55abbf067db3c6e9
Ivan Ivic <ivanivic842@gmail.com>
2024-03-19 10:13:51 +0100

    getCrawlInstances func

--- internal/data/crawl_instances.go
+++ internal/data/crawl_instances.go
@@ -1,6 +1,7 @@
 package data
 
 import (
+	"database/sql"
 	"sort"
 	"time"
 )
@@ -24,8 +25,43 @@ func (tfs TimeFrameSlice) Swap(i, j int) {
 	tfs[i], tfs[j] = tfs[j], tfs[i]
 }
 
-func GetCrawlInstances(from, to time.Time) TimeFrameSlice {
-	return nil
+func (s *Store) GetCrawlInstances(from, to time.Time) (TimeFrameSlice, error) {
+
+	db, err := sql.Open("libsql", s.DbUrl)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer db.Close()
+
+	query := `
+		SELECT since, until
+		FROM crawl_instances
+		WHERE since >= ?
+		AND until <= ?;
+		`
+
+	rows, err := db.Query(query, from, to)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var crawlInstances TimeFrameSlice
+
+	for rows.Next() {
+		var since, until time.Time
+		if err := rows.Scan(&since, &until); err != nil {
+			return nil, err
+		}
+		crawlInstances = append(crawlInstances, TimeFrame{Since: since, Until: until})
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return crawlInstances, nil
 }
 
 func FindGaps(from, to time.Time, timeFrames TimeFrameSlice) TimeFrameSlice {
@@ -33,14 +69,12 @@ func FindGaps(from, to time.Time, timeFrames TimeFrameSlice) TimeFrameSlice {
 
 	sort.Sort(timeFrames)
 
-
 	var until time.Time
 	if len(timeFrames) == 0 {
 		until = to
 	} else {
 		until = timeFrames[0].Since
 	}
-
 
 	gaps = append(gaps, TimeFrame{
 		Since: from,
@@ -59,5 +93,3 @@ func FindGaps(from, to time.Time, timeFrames TimeFrameSlice) TimeFrameSlice {
 	return gaps
 
 }
-
-
