e079d56638f364db2f8d200d7cc79a6038ae8a34
Ivan Ivic <ivanivic842@gmail.com>
2024-04-04 17:06:59 +0200

    formatted code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -81,6 +81,8 @@ func main() {
 	g.GET("/metrics/quality", app.QualityMetricsPage)
 	g.GET("/metrics/throughput", app.ThroughputMetricsPage)
 
+	g.GET("/:mrid/details", app.MergeRequestDetailsPage)
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
---
+++ internal/handler/merge_request_details.go
@@ -0,0 +1,80 @@
+package handler
+
+import (
+	"github.com/dxta-dev/app/internal/data"
+	"github.com/dxta-dev/app/internal/middleware"
+	"github.com/dxta-dev/app/internal/template"
+
+	"context"
+	"fmt"
+	"net/url"
+	"strconv"
+
+	"github.com/donseba/go-htmx"
+	"github.com/labstack/echo/v4"
+)
+
+func (a *App) MergeRequestDetailsPage(c echo.Context) error {
+	r := c.Request()
+	h := r.Context().Value(htmx.ContextRequestHeader).(htmx.HxRequestHeader)
+
+	tenantDatabaseUrl := r.Context().Value(middleware.TenantDatabaseURLContext).(string)
+
+	store := &data.Store{
+		DbUrl: tenantDatabaseUrl,
+	}
+
+	paramMrId := c.Param("mrid")
+	mrId, err := strconv.ParseInt(paramMrId, 10, 64)
+
+	if paramMrId == "" || err != nil {
+		return c.String(400, "")
+	}
+
+	parsedURL, err := url.Parse(h.HxCurrentURL)
+
+	if err != nil {
+		return err
+	}
+
+	week := parsedURL.Query().Get("week")
+
+	state := DashboardState{
+		week: week,
+		mr:   &mrId,
+	}
+
+	if team := parsedURL.Query().Get("team"); team != "" {
+		teamId, err := strconv.ParseInt(team, 10, 64)
+
+		if err != nil {
+			return err
+		}
+
+		a.State.Team = &teamId
+	}
+
+	nextUrl, err := getNextDashboardUrl(a, h.HxCurrentURL, state, nil, true)
+
+	if err != nil {
+		return err
+	}
+
+	c.Response().Header().Set("HX-Push-Url", nextUrl)
+
+	events, err := store.GetMergeRequestEvents(mrId)
+
+	if err != nil {
+		return err
+	}
+
+	mergeRequestInfoProps := template.MergeRequestInfoProps{
+		Events:         events,
+		DeleteEndpoint: fmt.Sprintf("/merge-request/%d", mrId),
+		TargetSelector: "#slide-over",
+	}
+
+	components := template.MergeRequestInfo(mergeRequestInfoProps)
+
+	return components.Render(context.Background(), c.Response().Writer)
+}
