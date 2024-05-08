07fd4298930e9c3b0cbed70b773103ef9a94d400
Dejan Tot <dejan@crocoder.dev>
2024-05-08 17:43:12 +0200

    feat: show more on mr stack list (#215)
    * add show more link
    
    * idk
    
    * changes
    
    * remove commented code
    
    * ?
    
    * remove?
    
    * limit avatar_urls to 5
    
    ---------
    
    Co-authored-by: David Abram <david@crocoder.dev>

--- internal/handler/dashboard.go
+++ internal/handler/dashboard.go
@@ -307,32 +307,47 @@ func (a *App) DashboardPage(c echo.Context) error {
 
 	isQueryCurrentWeek := util.GetFormattedWeek(date) == util.GetFormattedWeek(timeNow)
 	if isQueryCurrentWeek {
-		mergeRequestsInProgress.MergeRequests, err = store.GetMergeRequestsInProgress(date, teamMembers, nullRows.UserId)
+		mergeRequestsInProgress.Id = "mrs-in-progress"
+		mergeRequestsInProgress.Title = "In progress"
+		mergeRequestsInProgress.ShowMoreUrl = "/mr-stack/in-progress"
+		mergeRequestsInProgress.MergeRequests, err = store.GetMergeRequestInProgressCountedList(timeNow, teamMembers, nullRows.UserId)
 
 		if err != nil {
 			return err
 		}
 
-		mergeRequestsReadyToMerge.MergeRequests, err = store.GetMergeRequestsReadyToMerge(teamMembers, nullRows.UserId)
+		mergeRequestsReadyToMerge.Id = "mrs-ready-to-merge"
+		mergeRequestsReadyToMerge.Title = "Ready to merge"
+		mergeRequestsReadyToMerge.ShowMoreUrl = "/mr-stack/ready-to-merge"
+		mergeRequestsReadyToMerge.MergeRequests, err = store.GetMergeRequestReadyToMergeCountedList(teamMembers, nullRows.UserId)
 
 		if err != nil {
 			return err
 		}
 
-		mergeRequestsWaitingForReview.MergeRequests, err = store.GetMergeRequestsWaitingForReview(teamMembers, nullRows.UserId)
+		mergeRequestsWaitingForReview.Id = "mrs-waiting-review"
+		mergeRequestsWaitingForReview.Title = "Waiting for review"
+		mergeRequestsWaitingForReview.ShowMoreUrl = "/mr-stack/waiting-for-review"
+		mergeRequestsWaitingForReview.MergeRequests, err = store.GetMergeRequestWaitingForReviewCountedList(teamMembers, nullRows.UserId)
 
 		if err != nil {
 			return err
 		}
 	}
 
-	mergeRequestsMerged.MergeRequests, err = store.GetMergeRequestsClosed(date, teamMembers, nullRows.UserId, true)
+	mergeRequestsMerged.Id = "mrs-merged"
+	mergeRequestsMerged.Title = "Merged"
+	mergeRequestsMerged.ShowMoreUrl = "/mr-stack/merged"
+	mergeRequestsMerged.MergeRequests, err = store.GetMergeRequestMergedCountedList(date, teamMembers, nullRows.UserId)
 
 	if err != nil {
 		return err
 	}
 
-	mergeRequestsClosed.MergeRequests, err = store.GetMergeRequestsClosed(date, teamMembers, nullRows.UserId, false)
+	mergeRequestsClosed.Id = "mrs-closed"
+	mergeRequestsClosed.Title = "Closed"
+	mergeRequestsClosed.ShowMoreUrl = "/mr-stack/closed"
+	mergeRequestsClosed.MergeRequests, err = store.GetMergeRequestClosedCountedList(date, teamMembers, nullRows.UserId)
 
 	if err != nil {
 		return err
