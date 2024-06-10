1fc0adc1920c216c4db3e816fad58211cf309c28
Dejan Tot <dejan@crocoder.dev>
2024-06-10 15:09:27 +0200

    fix bug (#289)

--- internal/handler/dashboard.go
+++ internal/handler/dashboard.go
@@ -270,8 +270,9 @@ func (a *App) DashboardPage(c echo.Context) error {
 		}
 		eventMergeRequestIds = append(eventMergeRequestIds, event.MergeRequestId)
 	}
-	var startedMergeRequestIds []int64
-	var closedMergeRequestIds []int64
+	startedMergeRequestIds := []int64{}
+	closedMergeRequestIds := []int64{}
+
 	for id, _ := range mergeRequestsStartedThisWeek {
 		startedMergeRequestIds = append(startedMergeRequestIds, id)
 	}
@@ -299,11 +300,11 @@ func (a *App) DashboardPage(c echo.Context) error {
 		}
 
 		mergeRequestInfoProps = &template.MergeRequestInfoProps{
-			Events:         events,
-			UniqueDates:    uniqueDates,
-			DetailsPageUrl: fmt.Sprintf("/mr/%d", *state.mr),
-			ShouldOpenMrInfo:   !(h.HxBoosted && h.HxRequest) && state.mr != nil,
-			TargetSelector: "#mr-info",
+			Events:           events,
+			UniqueDates:      uniqueDates,
+			DetailsPageUrl:   fmt.Sprintf("/mr/%d", *state.mr),
+			ShouldOpenMrInfo: !(h.HxBoosted && h.HxRequest) && state.mr != nil,
+			TargetSelector:   "#mr-info",
 		}
 	}
 
