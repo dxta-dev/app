5b85ad73fd2ca2d09838ddac8966c9aa9a6270ad
David Abram <david@crocoder.dev>
2024-09-16 22:55:42 +0200

    mrsize query

--- internal/data/api/mr_size.go
+++ internal/data/api/mr_size.go
@@ -17,13 +17,15 @@ type MRSize struct {
 
 /*
 	SELECT
-		FLOOR(AVG(metrics.mr_size)) as AVG,
 		mergedAt.week as WEEK,
-		COUNT(*) AS C,
+		FLOOR(AVG(metrics.mr_size)) as AVG,
 		FLOOR(MEDIAN(metrics.mr_size)) AS P50,
 		FLOOR(PERCENTILE_75(metrics.mr_size)) AS P75,
-		FLOOR(PERCENTILE_95(metrics.mr_size)) as P95
+		FLOOR(PERCENTILE_95(metrics.mr_size)) as P95,
+		COUNT(*) AS C
 	FROM transform_merge_request_metrics AS metrics
+	JOIN transform_repositories AS repo
+	ON repo.id = metrics.repository
 	JOIN transform_merge_request_fact_dates_junk AS dj
 	ON metrics.dates_junk = dj.id
 	JOIN transform_dates AS mergedAt
@@ -33,11 +35,66 @@ type MRSize struct {
 	JOIN transform_forge_users AS author
 	ON uj.author = author.id
 	WHERE mergedAt.week IN ("2024-W26", "2024-W27", "2024-W28", "2024-W29", "2024-W30", "2024-W31", "2024-W32", "2024-W33", "2024-W34", "2024-W35", "2024-W36", "2024-W37")
+	AND repo.name = "cal.com"
+	AND repo.namespace_name = "calcom"
 	AND author.external_id in (SELECT member FROM tenant_team_members WHERE team = 1)
 	AND author.bot = 0
 	GROUP BY mergedAt.week;
 */
 
-func GetMRSize(db *sql.DB, weeks []string, team *int64) (map[string]MRSize, error) {
+func GetMRSize(db *sql.DB, namespace string, repository string, weeks []string, team *int64) (map[string]MRSize, error) {
+
+	teamQuery := ""
+	queryParamLength := len(weeks) + 1 /* repository name */ + 1 /* repository namespace */
+
+	if team != nil {
+		teamQuery = "AND author.external_id in (SELECT member FROM tenant_team_members WHERE team = ?)"
+		queryParamLength += 1
+	}
+
+	weeksPlaceholder := strings.Repeat("?,", len(weeks)-1) + "?"
+
+	queryParams := make([]interface{}, queryParamLength)
+	for i, v := range weeks {
+		queryParams[i] = v
+	}
+
+	queryParams = append(queryParams, namespace)
+	queryParams = append(queryParams, repository)
+
+	if team != nil {
+		queryParams = append(queryParams, team)
+	}
+
+	_ = fmt.Sprintf(`
+	SELECT
+		mergedAt.week as WEEK,
+		FLOOR(AVG(metrics.mr_size)) as AVG,
+		FLOOR(MEDIAN(metrics.mr_size)) AS P50,
+		FLOOR(PERCENTILE_75(metrics.mr_size)) AS P75,
+		FLOOR(PERCENTILE_95(metrics.mr_size)) as P95,
+		COUNT(*) AS C
+	FROM transform_merge_request_metrics AS metrics
+	JOIN transform_repositories AS repo
+	ON repo.id = metrics.repository
+	JOIN transform_merge_request_fact_dates_junk AS dj
+	ON metrics.dates_junk = dj.id
+	JOIN transform_dates AS mergedAt
+	ON dj.merged_at = mergedAt.id
+	JOIN transform_merge_request_fact_users_junk AS uj
+	ON metrics.users_junk = uj.id
+	JOIN transform_forge_users AS author
+	ON uj.author = author.id
+	WHERE mergedAt.week IN (%s)
+	AND repo.namespace_name = ?
+	AND repo.name = ?
+	%s
+	AND author.bot = 0
+	GROUP BY mergedAt.week;
+	`,
+		weeksPlaceholder,
+		teamQuery,
+	)
+
 	return nil, nil
 }
