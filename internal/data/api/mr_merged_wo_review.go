92503185338887a807f0f926a2cdcf55e50ab3a1
David Abram <david@crocoder.dev>
2024-09-17 08:57:48 +0200

    mrs merged without review
    :usa:

--- internal/data/api/mr_merged_wo_review.go
+++ internal/data/api/mr_merged_wo_review.go
@@ -7,30 +7,31 @@ import (
 )
 
 type MRsMergedWithoutReview struct {
-	Week         string
-	Average      int
-	Median       int
-	Percentile75 int
-	Percentile95 int
-	Count        int
+	Week  string
+	Count int
 }
 
 /*
 	SELECT
-		COUNT(*),
-		mergedAt.week
+		mergedAt.week,
+		COUNT(*)
 	FROM transform_merge_request_metrics AS metrics
 	JOIN transform_merge_request_fact_dates_junk AS dj
 	ON metrics.dates_junk = dj.id
+	JOIN transform_repositories AS repo
+	ON repo.id = metrics.repository
 	JOIN transform_dates AS mergedAt
 	ON dj.merged_at = mergedAt.id
 	JOIN transform_merge_request_fact_users_junk AS uj
 	ON metrics.users_junk = uj.id
 	JOIN transform_forge_users AS author
 	ON uj.author = author.id
-	WHERE mergedAt.week IN (%s) and metrics.review_depth = 0
+	WHERE mergedAt.week IN ("2024-W26", "2024-W27", "2024-W28", "2024-W29", "2024-W30", "2024-W31", "2024-W32", "2024-W33", "2024-W34", "2024-W35", "2024-W36", "2024-W37")
+	AND metrics.review_depth = 0
+	AND repo.name = "cal.com"
+	AND repo.namespace_name = "calcom"
+	AND author.external_id in (SELECT member FROM tenant_team_members WHERE team = 1)
 	AND author.bot = 0
-	%s
 	GROUP BY mergedAt.week;
 */
 
@@ -60,15 +61,13 @@ func GetMRsMergedWithoutReview(db *sql.DB, namespace string, repository string, 
 
 	_ = fmt.Sprintf(`
 	SELECT
-		mergedAt.week as WEEK,
-		FLOOR(AVG(metrics.mr_size)) as AVG,
-		FLOOR(MEDIAN(metrics.mr_size)) AS P50,
-		FLOOR(PERCENTILE_75(metrics.mr_size)) AS P75,
-		FLOOR(PERCENTILE_95(metrics.mr_size)) as P95,
-		COUNT(*) AS C
+		mergedAt.week,
+		COUNT(*)
 	FROM transform_merge_request_metrics AS metrics
 	JOIN transform_merge_request_fact_dates_junk AS dj
 	ON metrics.dates_junk = dj.id
+	JOIN transform_repositories AS repo
+	ON repo.id = metrics.repository
 	JOIN transform_dates AS mergedAt
 	ON dj.merged_at = mergedAt.id
 	JOIN transform_merge_request_fact_users_junk AS uj
@@ -76,8 +75,9 @@ func GetMRsMergedWithoutReview(db *sql.DB, namespace string, repository string, 
 	JOIN transform_forge_users AS author
 	ON uj.author = author.id
 	WHERE mergedAt.week IN (%s)
-	AND repo.namespace_name = ?
+	AND metrics.review_depth = 0
 	AND repo.name = ?
+	AND repo.namespace_name = ?
 	%s
 	AND author.bot = 0
 	GROUP BY mergedAt.week;
