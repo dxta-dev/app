b1655e57e2d16ca1c59f9b0ce58ccf4cb3917e8a
Ivan Ivic <ivanivic842@gmail.com>
2024-09-17 08:34:04 +0200

    more quality metrics

---
+++ internal/data/api/review_depth.go
@@ -0,0 +1,89 @@
+package api
+
+import (
+	"database/sql"
+	"fmt"
+	"strings"
+)
+
+type MRReviewDepth struct {
+	Week         string
+	Average      int
+	Median       int
+	Percentile75 int
+	Percentile95 int
+	Count        int
+}
+
+/*
+	SELECT
+		AVG(metrics.review_depth),
+		mergedAt.week
+	FROM transform_merge_request_metrics AS metrics
+	JOIN transform_merge_request_fact_dates_junk AS dj
+	ON metrics.dates_junk = dj.id
+	JOIN transform_dates AS mergedAt
+	ON dj.merged_at = mergedAt.id
+	JOIN transform_merge_request_fact_users_junk AS uj
+	ON metrics.users_junk = uj.id
+	JOIN transform_forge_users AS author
+	ON uj.author = author.id
+	WHERE mergedAt.week IN (%s)
+	AND author.bot = 0
+	%s
+	GROUP BY mergedAt.week;
+*/
+
+func GetMRReviewDepth(db *sql.DB, namespace string, repository string, weeks []string, team *int64) (map[string]MRSize, error) {
+
+	teamQuery := ""
+	queryParamLength := len(weeks) + 1 /* repository name */ + 1 /* repository namespace */
+
+	if team != nil {
+		teamQuery = "AND author.external_id in (SELECT member FROM tenant_team_members WHERE team = ?)"
+		queryParamLength += 1
+	}
+
+	weeksPlaceholder := strings.Repeat("?,", len(weeks)-1) + "?"
+
+	queryParams := make([]interface{}, queryParamLength)
+	for i, v := range weeks {
+		queryParams[i] = v
+	}
+
+	queryParams = append(queryParams, namespace)
+	queryParams = append(queryParams, repository)
+
+	if team != nil {
+		queryParams = append(queryParams, team)
+	}
+
+	_ = fmt.Sprintf(`
+	SELECT
+		mergedAt.week as WEEK,
+		FLOOR(AVG(metrics.mr_size)) as AVG,
+		FLOOR(MEDIAN(metrics.mr_size)) AS P50,
+		FLOOR(PERCENTILE_75(metrics.mr_size)) AS P75,
+		FLOOR(PERCENTILE_95(metrics.mr_size)) as P95,
+	FROM transform_merge_request_metrics AS metrics
+	JOIN transform_merge_request_fact_dates_junk AS dj
+	ON metrics.dates_junk = dj.id
+	JOIN transform_dates AS mergedAt
+	ON dj.merged_at = mergedAt.id
+	JOIN transform_merge_request_fact_users_junk AS uj
+	ON metrics.users_junk = uj.id
+	JOIN transform_forge_users AS author
+	ON uj.author = author.id
+	WHERE mergedAt.week IN (%s)
+	AND repo.namespace_name = ?
+	AND repo.name = ?
+	%s
+	AND author.bot = 0
+	GROUP BY mergedAt.week;
+	`,
+		weeksPlaceholder,
+		teamQuery,
+	)
+
+	return nil, nil
+}
