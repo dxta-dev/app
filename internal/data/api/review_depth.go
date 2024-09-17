8c3a956cee8a8b43882b108258192260aec86582
David Abram <david@crocoder.dev>
2024-09-17 09:33:13 +0200

    mr review depth

--- internal/data/api/review_depth.go
+++ internal/data/api/review_depth.go
@@ -1,25 +1,24 @@
 package api
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"strings"
 )
 
 type MRReviewDepth struct {
-	Week         string
-	Average      int
-	Median       int
-	Percentile75 int
-	Percentile95 int
-	Count        int
+	Week  string
+	Value float64
 }
 
 /*
 	SELECT
-		AVG(metrics.review_depth),
-		mergedAt.week
+		mergedAt.week as WEEK,
+		AVG(metrics.review_depth) as AVG
 	FROM transform_merge_request_metrics AS metrics
+	JOIN transform_repositories AS repo
+	ON repo.id = metrics.repository
 	JOIN transform_merge_request_fact_dates_junk AS dj
 	ON metrics.dates_junk = dj.id
 	JOIN transform_dates AS mergedAt
@@ -28,13 +27,15 @@ type MRReviewDepth struct {
 	ON metrics.users_junk = uj.id
 	JOIN transform_forge_users AS author
 	ON uj.author = author.id
-	WHERE mergedAt.week IN (%s)
+	WHERE mergedAt.week IN ("2024-W26", "2024-W27", "2024-W28", "2024-W29", "2024-W30", "2024-W31", "2024-W32", "2024-W33", "2024-W34", "2024-W35", "2024-W36", "2024-W37")
+	AND repo.name = "cal.com"
+	AND repo.namespace_name = "calcom"
+	AND author.external_id in (SELECT member FROM tenant_team_members WHERE team = 1)
 	AND author.bot = 0
-	%s
 	GROUP BY mergedAt.week;
 */
 
-func GetMRReviewDepth(db *sql.DB, namespace string, repository string, weeks []string, team *int64) (map[string]MRSize, error) {
+func GetMRReviewDepth(db *sql.DB, ctx context.Context, namespace string, repository string, weeks []string, team *int64) (map[string]MRReviewDepth, error) {
 
 	teamQuery := ""
 	queryParamLength := len(weeks) + 1 /* repository name */ + 1 /* repository namespace */
@@ -58,14 +59,13 @@ func GetMRReviewDepth(db *sql.DB, namespace string, repository string, weeks []s
 		queryParams = append(queryParams, team)
 	}
 
-	_ = fmt.Sprintf(`
+	query := fmt.Sprintf(`
 	SELECT
 		mergedAt.week as WEEK,
-		FLOOR(AVG(metrics.mr_size)) as AVG,
-		FLOOR(MEDIAN(metrics.mr_size)) AS P50,
-		FLOOR(PERCENTILE_75(metrics.mr_size)) AS P75,
-		FLOOR(PERCENTILE_95(metrics.mr_size)) as P95,
+		AVG(metrics.review_depth) as AVG
 	FROM transform_merge_request_metrics AS metrics
+	JOIN transform_repositories AS repo
+	ON repo.id = metrics.repository
 	JOIN transform_merge_request_fact_dates_junk AS dj
 	ON metrics.dates_junk = dj.id
 	JOIN transform_dates AS mergedAt
@@ -75,8 +75,8 @@ func GetMRReviewDepth(db *sql.DB, namespace string, repository string, weeks []s
 	JOIN transform_forge_users AS author
 	ON uj.author = author.id
 	WHERE mergedAt.week IN (%s)
-	AND repo.namespace_name = ?
 	AND repo.name = ?
+	AND repo.namespace_name = ?
 	%s
 	AND author.bot = 0
 	GROUP BY mergedAt.week;
@@ -85,5 +85,28 @@ func GetMRReviewDepth(db *sql.DB, namespace string, repository string, weeks []s
 		teamQuery,
 	)
 
-	return nil, nil
+	rows, err := db.QueryContext(ctx, query, queryParams...)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer rows.Close()
+
+	mrReviewDepthByWeek := make(map[string]MRReviewDepth)
+
+	for rows.Next() {
+		var mrReviewDepth MRReviewDepth
+
+		if err := rows.Scan(
+			&mrReviewDepth.Week,
+			&mrReviewDepth.Value,
+		); err != nil {
+			return nil, err
+		}
+
+		mrReviewDepthByWeek[mrReviewDepth.Week] = mrReviewDepth
+	}
+
+	return mrReviewDepthByWeek, nil
 }
