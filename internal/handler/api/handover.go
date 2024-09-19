bbed635e6421321105975bde69c7dcf9993b6387
David Abram <david@crocoder.dev>
2024-09-20 00:17:56 +0200

    hanlder ctx

--- internal/handler/api/handover.go
+++ internal/handler/api/handover.go
@@ -1,7 +1,6 @@
 package api
 
 import (
-	"context"
 	"net/http"
 	"time"
 
@@ -11,6 +10,8 @@ import (
 )
 
 func HandoverHandler(c echo.Context) error {
+
+	ctx := c.Request().Context()
 
 	apiState, err := NewAPIState(c)
 
@@ -22,7 +23,7 @@ func HandoverHandler(c echo.Context) error {
 
 	weeks := util.GetLastNWeeks(time.Now(), 3*4)
 
-	handovers, err := api.GetHandover(apiState.DB, context.Background(), apiState.org, apiState.repo, weeks, apiState.teamId)
+	handovers, err := api.GetHandover(apiState.DB, ctx, apiState.org, apiState.repo, weeks, apiState.teamId)
 
 	if err != nil {
 		return err
