687e710f4f4c2aa327ec842a69a72b0dbee3e9a4
Ivan Ivic <ivanivic842@gmail.com>
2024-09-19 09:54:06 +0200

    refactor cycle time and handover handlers

--- internal/data/api/cycle_time.go
+++ internal/data/api/cycle_time.go
@@ -8,11 +8,11 @@ import (
 )
 
 type CycleTime struct {
-	Week         string `json:"week"`
-	Average      int    `json:"average"`
-	Median       int    `json:"median"`
-	Percentile75 int    `json:"percentile75"`
-	Percentile95 int    `json:"percentile95"`
+	Week         string  `json:"week"`
+	Average      float64 `json:"average"`
+	Median       float64 `json:"median"`
+	Percentile75 float64 `json:"percentile75"`
+	Percentile95 float64 `json:"percentile95"`
 }
 
 /*
@@ -52,7 +52,7 @@ func GetCycleTime(db *sql.DB, ctx context.Context, namespace string, repository 
 		queryParamLength += 1
 	}
 
-	weeksPlaceholder := strings.Repeat("?,", queryParamLength)
+	weeksPlaceholder := strings.Repeat("?,", len(weeks)-1) + "?"
 
 	queryParams := make([]interface{}, queryParamLength)
 
