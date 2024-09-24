c5f9c1c046df5b8137c96b8676fe26e942c55469
Ivan Ivic <ivanivic842@gmail.com>
2024-09-24 11:51:07 +0200

    time to merge data fetch and handler (#459)

--- internal/handler/api/time_to_merge.go
+++ internal/handler/api/time_to_merge.go
@@ -1,25 +1,31 @@
 package api
 
 import (
-	"fmt"
 	"net/http"
-	"strings"
 	"time"
 
+	"github.com/dxta-dev/app/internal/data/api"
 	"github.com/dxta-dev/app/internal/util"
 	"github.com/labstack/echo/v4"
 )
 
 func TimeToMergeHandler(c echo.Context) error {
-	org := c.Param("org")
-	repo := c.Param("repo")
 
-	team := c.QueryParam("team")
+	ctx := c.Request().Context()
+
+	apiState, err := NewAPIState(c)
+
+	if err != nil {
+		return err
+	}
 
 	weeks := util.GetLastNWeeks(time.Now(), 3*4)
 
-	response := fmt.Sprintf("Organization: %s\nRepository: %s\nTeam: %s\nWeeks: %s",
-		org, repo, team, strings.Join(weeks, ", "))
+	timesToMerge, err := api.GetTimeToMerge(apiState.DB, ctx, apiState.org, apiState.repo, weeks, apiState.teamId)
 
-	return c.String(http.StatusOK, response)
+	if err != nil {
+		return err
+	}
+
+	return c.JSON(http.StatusOK, timesToMerge)
 }
