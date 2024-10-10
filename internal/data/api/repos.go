81aa46f7f670f02e71dfe6b264510d043b47be3f
David Abram <david@crocoder.dev>
2024-10-10 15:13:36 +0200

    added some more stuff

--- internal/data/api/repos.go
+++ internal/data/api/repos.go
@@ -6,12 +6,14 @@ import (
 )
 
 type Repo struct {
-	Organization string `json:"organization"`
-	Repository   string `json:"repository"`
+	Organization       string `json:"organization"`
+	Repository         string `json:"repository"`
+	ProjectName        string `json:"projectName"`
+	ProjectDescription string `json:"projectDescription"`
 }
 
 func GetRepos(ctx context.Context, db *sql.DB) ([]Repo, error) {
-	query := `SELECT organization, repository FROM repos`
+	query := `SELECT organization, repository, project_name, project_description FROM repos`
 	rows, err := db.QueryContext(ctx, query)
 	if err != nil {
 		return nil, err
@@ -21,7 +23,7 @@ func GetRepos(ctx context.Context, db *sql.DB) ([]Repo, error) {
 	var repos []Repo
 	for rows.Next() {
 		var repo Repo
-		err := rows.Scan(&repo.Organization, &repo.Repository)
+		err := rows.Scan(&repo.Organization, &repo.Repository, &repo.ProjectName, &repo.ProjectDescription)
 		if err != nil {
 			return nil, err
 		}
