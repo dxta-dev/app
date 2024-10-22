ebebb3e9713bceb9f9a7db928b027d839359b1c7
dejan-crocoder <dejan@crocoder.dev>
2024-10-22 11:23:15 +0200

    add author not bot in code change

--- internal/data/api/code_change.go
+++ internal/data/api/code_change.go
@@ -63,24 +63,25 @@ func GetCodeChanges(db *sql.DB, ctx context.Context, namespace string, repositor
 		COUNT(metrics.mr_size) AS COUNT
 	FROM transform_merge_request_metrics AS metrics
 	JOIN transform_repositories AS repo
-	ON repo.id = metrics.repository
+		ON repo.id = metrics.repository
 	JOIN transform_merge_request_fact_dates_junk AS dates_junk
-	ON metrics.dates_junk = dates_junk.id
+		ON metrics.dates_junk = dates_junk.id
 	JOIN transform_dates AS dates
-	ON dates_junk.merged_at = dates.id
+		ON dates_junk.merged_at = dates.id
 	JOIN transform_merge_request_fact_users_junk AS uj
-	ON metrics.users_junk = uj.id
+		ON metrics.users_junk = uj.id
 	JOIN transform_forge_users AS author
-	ON uj.author = author.id
+		ON uj.author = author.id
 	JOIN transform_merge_requests AS mrs
-	ON metrics.merge_request = mrs.id
+		ON metrics.merge_request = mrs.id
 	JOIN transform_branches AS branch
-	ON mrs.target_branch = branch.id	
+		ON mrs.target_branch = branch.id	
 	WHERE dates.week IN (%s)
 	AND repo.namespace_name = ?
 	AND repo.name = ?
 	AND branch.name = 'main'
-	%s
+		%s
+	AND author.bot = 0
 	GROUP BY dates.week
 	ORDER BY dates.week ASC`,
 		weeksPlaceholder,
