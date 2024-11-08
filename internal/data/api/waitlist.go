31a1317e04ae335a14a7c933cadc2ccf7e05a87c
Ivan Ivic <ivanivic842@gmail.com>
2024-11-08 15:18:17 +0100

    do not replace, DO NOTHING

--- internal/data/api/waitlist.go
+++ internal/data/api/waitlist.go
@@ -8,8 +8,8 @@ import (
 
 func InsertWaitlistData(db *sql.DB, ctx context.Context, userEmail string, repoUrl string) error {
 	query := `
-		INSERT OR REPLACE INTO users_waitlist (user_email, repository_url)
-		VALUES (?, ?)
+		INSERT INTO users_waitlist (user_email, repository_url)
+		VALUES (?, ?) ON CONFLICT (user_email, repository_url) DO NOTHING
 	`
 	_, err := db.ExecContext(ctx, query, userEmail, repoUrl)
 	if err != nil {
