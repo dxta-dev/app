6362d5477b8daed89f2fa2ee80c2143f44dfb640
David Abram <david@crocoder.dev>
2024-12-08 17:49:00 +0100

    fix

--- internal/api/handler/teams.go
+++ internal/api/handler/teams.go
@@ -3,20 +3,20 @@ package handler
 import (
 	"net/http"
 
-	"github.com/dxta-dev/app/internal/api/data"
+	"github.com/dxta-dev/app/internal/api"
 	"github.com/labstack/echo/v4"
 )
 
 func TeamsHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 
-	apiState, err := NewAPIState(c)
+	apiState, err := api.NewAPIState(c)
 
 	if err != nil {
 		return err
 	}
 
-	teams, err := data.GetTeams(apiState.DB, ctx)
+	teams, err := apiState.DB.GetTeams(ctx)
 
 	if err != nil {
 		return err
