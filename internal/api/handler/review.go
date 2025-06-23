866de27de9531796f095781f306c97039ea8862c
davidabram <david@crocoder.dev>
2025-06-24 00:24:55 +0200

    the rest of values

--- internal/api/handler/review.go
+++ internal/api/handler/review.go
@@ -6,8 +6,57 @@ import (
 
 	"github.com/dxta-dev/app/internal/api"
 	"github.com/dxta-dev/app/internal/data"
+	"github.com/dxta-dev/app/internal/markdown"
 	"github.com/dxta-dev/app/internal/util"
 )
+
+func ReviewMarkdownHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
+
+	apiState, err := api.NewAPIState(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	weekParam := r.URL.Query().Get("weeks")
+
+	weeksArray := util.GetWeeksArray(weekParam)
+	weeksSorted := util.SortISOWeeks(weeksArray)
+
+	query := data.BuildReviewQuery(weeksSorted, apiState.TeamId)
+
+	result, err := apiState.DB.GetAggregatedValues(
+		ctx,
+		query,
+		apiState.Org,
+		apiState.Repo,
+		weeksSorted,
+		apiState.TeamId,
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	m, err := markdown.GetAggregatedValuesMarkdown(
+		ctx,
+		"Reviewed Metrics",
+		``,
+		result,
+	)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/markdown; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	if _, err := w.Write([]byte(m)); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
 
 func ReviewHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
