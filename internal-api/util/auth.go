72b9172d07389dace823235ea86152a312943cec
stefanskoricdev <stephanskoric@gmail.com>
2025-06-23 17:10:29 +0200

    refactor: Auth utils refactor

--- internal-api/util/auth.go
+++ internal-api/util/auth.go
@@ -24,25 +24,7 @@ type JWKS struct {
 	Keys []Key `json:"keys"`
 }
 
-func CreateAuthVerifier(pubKey ed25519.PublicKey) *jwtauth.JWTAuth {
-	tokenAuth := jwtauth.New("EdDSA", nil, pubKey)
-	return tokenAuth
-}
-
-func GetRawPublicKeyFromJWK(jwkData Key) (ed25519.PublicKey, error) {
-	decodedKey, err := base64.RawURLEncoding.DecodeString(jwkData.X)
-	if err != nil {
-		return nil, fmt.Errorf("failed to base64url decode public key material: %w", err)
-	}
-
-	if len(decodedKey) != ed25519.PublicKeySize {
-		return nil, fmt.Errorf("invalid Ed25519 public key length: got %d, want %d", len(decodedKey), ed25519.PublicKeySize)
-	}
-
-	return ed25519.PublicKey(decodedKey), nil
-}
-
-func RetrieveJWK() (Key, error) {
+func retrieveJWK() (Key, error) {
 	resp, err := http.Get(os.Getenv("AUTH_JWKS_URL"))
 
 	if err != nil {
@@ -67,6 +49,42 @@ func RetrieveJWK() (Key, error) {
 	}
 
 	return jwks.Keys[0], nil
+}
+
+func getRawPublicKeyFromJWK(jwkData Key) (ed25519.PublicKey, error) {
+	decodedKey, err := base64.RawURLEncoding.DecodeString(jwkData.X)
+	if err != nil {
+		return nil, fmt.Errorf("failed to base64url decode public key material: %w", err)
+	}
+
+	if len(decodedKey) != ed25519.PublicKeySize {
+		return nil, fmt.Errorf("invalid Ed25519 public key length: got %d, want %d", len(decodedKey), ed25519.PublicKeySize)
+	}
+
+	return ed25519.PublicKey(decodedKey), nil
+}
+
+func GetRawPublicKey() (ed25519.PublicKey, error) {
+	jwkData, err := retrieveJWK()
+
+	if err != nil {
+		fmt.Println("Could not retrieve JWK")
+		return nil, err
+	}
+
+	fmt.Println("Could not get public key from JWK")
+	pubKey, err := getRawPublicKeyFromJWK(jwkData)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return pubKey, nil
+}
+
+func CreateAuthVerifier(pubKey ed25519.PublicKey) *jwtauth.JWTAuth {
+	tokenAuth := jwtauth.New("EdDSA", nil, pubKey)
+	return tokenAuth
 }
 
 type contextKey struct {
